c2939b6444464b377afa3e58d987a920f0a1e984
Shefaul Islam Shefa <sishefa19@gmail.com>
2025-11-22 00:56:16 +0600

    Upload Python solution for LeetCode 1748 & 2206

---
+++ easy/2025-11-21/1748_sum_of_unique_elements.py
@@ -0,0 +1,31 @@
+"""You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation: The unique elements are [1,2,3,4,5], and the sum is 15."""
+
+
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        sum = 0
+        for n in nums:
+            if nums.count(n) == 1:
+                sum += n
+
+        return sum
