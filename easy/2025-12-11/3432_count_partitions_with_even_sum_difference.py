b66ba2dd15424c7b876242bfd578f19f1241fc59
Shefaul Islam Shefa <sishefa19@gmail.com>
2025-12-12 02:17:08 +0600

    Upload Python solution for LeetCode 1021 & 3432

---
+++ easy/2025-12-11/3432_count_partitions_with_even_sum_difference.py
@@ -0,0 +1,68 @@
+'''You are given an integer array nums of length n.
+
+A partition is defined as an index i where 0 <= i < n - 1, splitting the array into two non-empty subarrays such that:
+
+Left subarray contains indices [0, i].
+Right subarray contains indices [i + 1, n - 1].
+Return the number of partitions where the difference between the sum of the left and right subarrays is even.
+
+ 
+
+Example 1:
+
+Input: nums = [10,10,3,7,6]
+
+Output: 4
+
+Explanation:
+
+The 4 partitions are:
+
+[10], [10, 3, 7, 6] with a sum difference of 10 - 26 = -16, which is even.
+[10, 10], [3, 7, 6] with a sum difference of 20 - 16 = 4, which is even.
+[10, 10, 3], [7, 6] with a sum difference of 23 - 13 = 10, which is even.
+[10, 10, 3, 7], [6] with a sum difference of 30 - 6 = 24, which is even.
+Example 2:
+
+Input: nums = [1,2,2]
+
+Output: 0
+
+Explanation:
+
+No partition results in an even sum difference.
+
+Example 3:
+
+Input: nums = [2,4,6,8]
+
+Output: 3
+
+Explanation:
+
+All partitions result in an even sum difference.
+
+ 
+
+Constraints:
+
+2 <= n == nums.length <= 100
+1 <= nums[i] <= 100'''
+
+
+class Solution:
+    def countPartitions(self, nums: List[int]) -> int:
+        total_sum = sum(nums)
+        sum_of_left_subarray = 0
+        count = 0
+
+        for i in range(len(nums)-1):
+            sum_of_left_subarray += nums[i]
+            sum_of_right_subarray = total_sum - sum_of_left_subarray
+            
+            difference = abs(sum_of_left_subarray - sum_of_right_subarray)
+            
+            if difference % 2 == 0:
+                count += 1
+        
+        return count
