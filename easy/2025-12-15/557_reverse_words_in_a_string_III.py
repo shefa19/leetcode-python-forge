ebf7c76eff5e7afd6db4f452f4949c926b15e962
Shefaul Islam Shefa <sishefa19@gmail.com>
2025-12-17 21:34:26 +0600

    Upload Python solution for LeetCode 3042 & 557

---
+++ easy/2025-12-15/557_reverse_words_in_a_string_III.py
@@ -0,0 +1,30 @@
+'''Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+
+Input: s = "Mr Ding"
+Output: "rM gniD"
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+s contains printable ASCII characters.
+s does not contain any leading or trailing spaces.
+There is at least one word in s.
+All the words in s are separated by a single space.'''
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        revers_s = s[::-1]
+
+        words =revers_s.split()
+
+        return " ".join(words[::-1])
